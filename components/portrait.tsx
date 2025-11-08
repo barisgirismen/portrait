db61d6cf5c12c1fdada260e4c1d18e258063da6c
Barış Girişmen <57728214+barisgirismen@users.noreply.github.com>
2025-11-08 13:26:05 +0300

    Refactor Portrait component by removing unused background layer and simplifying structure

--- components/portrait.tsx
+++ components/portrait.tsx
@@ -2,7 +2,6 @@
 
 import ImageDepthMap from '@/packages/react-depth-map-component/src/ImageDepthMap';
 
-const CLEAN_BACKGROUND_PATH = "/bg.jpg";
 const PORTRAIT_PATH = "/portrait.webp";
 const DEPTH_MAP_PATH = "/depthmap.webp";
 
@@ -23,33 +22,7 @@ export default function Portrait({
         overflow: "hidden",
       }}
     >
-      {/* Background Layer - This will now move*/}
-      <div>
         <ImageDepthMap
-          originalImg={CLEAN_BACKGROUND_PATH}
-          depthImg={DEPTH_MAP_PATH}
-          verticalThreshold={15}
-          horizontalThreshold={15}
-          respondTo="mouseMove"
-          reverseMotion={undefined}
-          className={undefined}
-          style={undefined}
-          onPermissionChange={undefined}
-        />
-      </div>
-
-      {/* Portrait Layer */}
-      <div 
-        style={{ 
-          position: "absolute", 
-          top: 0, 
-          left: 0, 
-          width: "100%", 
-          height: "100%",
-        }}
-      >
-        <div>
-          <ImageDepthMap
             originalImg={PORTRAIT_PATH}
             depthImg={DEPTH_MAP_PATH}
             verticalThreshold={15}
@@ -59,9 +32,7 @@ export default function Portrait({
             className={undefined}
             style={undefined}
             onPermissionChange={undefined}
-          />
-        </div>
-      </div>
+        />
     </div>
   );
 }
